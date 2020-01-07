7f49dfbb67c0afb4e2b0cc1cc67a250cad8d7249
Jacob <Jacalz@users.noreply.github.com>
2020-01-07 17:33:47 +0100

    Make the python code a bit better

--- python-projects/lagomvinklade/main.py
+++ python-projects/lagomvinklade/main.py
@@ -1,11 +1,16 @@
 import math
 
-n = 30
+# Amount of triangles to calculate. Bigger number equals to longer time to completion.
+n = 3000
+
+# Just define the c variable to zero initally.
 c = 0
 
+# Loop through and calculate all the triangles.
 for a in range(1, n+1, 1):
     for b in range(a, min(a*a+1, n+1), 1):
       if math.floor(math.sqrt(a*a+b*b-a*b)) == math.ceil((math.sqrt(a*a+b*b-a*b))):
         c += 1
 
+# We lastly print the amount of possible traingles withing the n number of triangles.
 print(c)
